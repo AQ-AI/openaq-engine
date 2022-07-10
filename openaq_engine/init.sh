3f69df1ce35ed23ee79654a61c26fe93868134a8
ChristinaLast <christina.last@outlook.com>
2022-07-10 16:15:13 -0400

    adding bash commands for WIP setup

--- openaq_engine/init.sh
+++ openaq_engine/init.sh
@@ -1,1 +1,3 @@
-aws configure
+ aws configure --profile christina_last
+ 
+ aws sns subscribe --topic-arn arn:aws:sns:us-east-1:470049585876:NewFetchObject --protocol lambda --notification-endpoint arn:aws:lambda:us-east-1:399143675412:function:openaq-realtime-function  --profile christina_last
