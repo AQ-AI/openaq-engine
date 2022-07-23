ef8ac46373ca33a5ca51ff96ce2cc84a82f10791
ChristinaLast <christina.last@outlook.com>
2022-07-23 05:17:55 -0400

    adjusting model settings

--- openaq_engine/config/model_settings.py
+++ openaq_engine/config/model_settings.py
@@ -39,17 +39,24 @@ class RealtimeOpenAQConfig:
 
 @dataclass
 class TimeSplitterConfig:
-    DATE_COL: str = "triage_datetime"
+    DATE_COL: str = "date.utc"
     TIME_WINDOW_LENGTH: int = 12
     WITHIN_WINDOW_SAMPLER: int = 3
     WINDOW_COUNT: int = 3  # this will increase for more than one split
     TABLE_NAME: str = "openaq"
     REGION = "us-east-1"
+    DATABASE = os.getenv("DB_NAME_OPENAQ")
     AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
     AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
     S3_BUCKET = os.getenv("S3_BUCKET_OPENAQ")
     S3_OUTPUT = os.getenv("S3_OUTPUT_OPENAQ")
     RESOURCE = boto3.resource("s3")
+    TRAIN_VALIDATION_DICT: Dict[str, List[Any]] = field(
+        default_factory=lambda: dict(
+            validation=[],
+            training=[],
+        )
+    )
 
 
 @dataclass
