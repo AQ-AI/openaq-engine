4f82170b0d983fd2b7c79f57baafc1466a654661
ChristinaLast <christina.last@outlook.com>
2022-11-01 23:42:09 +0000

    adding cohort filter

--- openaq_engine/src/preprocessing/cohort_filter.py
+++ openaq_engine/src/preprocessing/cohort_filter.py
@@ -3,6 +3,30 @@ import pandas as pd
 
 class Filter:
     @staticmethod
+    def filter_pollutant(df: pd.DataFrame, select_pollutant: str) -> pd.DataFrame:
+
+        """
+        Filter for rows selected pollutant
+
+        Parameters
+        ----------
+        df : pd.DataFrame
+            Dataframe with selected `pollutant`
+        """
+
+        return (
+            df.assign(
+                selected_pollutant=(
+                    df.parameter.apply(
+                        lambda pollutant: select_pollutant in str(pollutant)
+                    )
+                )
+            )
+            .query("selected_pollutant == True")
+            .drop(["selected_pollutant"], axis=1)
+        )
+
+    @staticmethod
     def filter_non_null_pm25_values(df: pd.DataFrame) -> pd.DataFrame:
         """
         Filter out rows which are non null
@@ -40,4 +64,55 @@ class Filter:
             )
             .query("extreme_values == False")
             .drop(["extreme_values"], axis=1)
+        )
+    @staticmethod
+    def filter_countries(df: pd.DataFrame, countries: List[str]) -> pd.DataFrame:
+        """
+        Filter for countries
+
+        Parameters
+        ----------
+        df : pd.DataFrame
+        countries: list with `countries`
+        """
+
+
+        return (
+            df.assign(
+                filtered_country=(
+                    df.country.apply(
+                        lambda country: any(
+                            str_ in country[1:-1].split(",") for str_ in countries
+                        )
+                    )
+                )
+            )
+            .query("filtered_country == True")
+            .drop(["filtered_country"], axis=1)
+        )
+    
+    @staticmethod
+    def filter_cities(df: pd.DataFrame, cities: List[str]) -> pd.DataFrame:
+        """
+        Filter for cities
+
+        Parameters
+        ----------
+        df : pd.DataFrame
+        cities: list with `cities`
+        """
+
+
+        return (
+            df.assign(
+                filtered_cities=(
+                    df.city.apply(
+                        lambda city: any(
+                            str_ in city[1:-1].split(",") for str_ in cities
+                        )
+                    )
+                )
+            )
+            .query("filtered_cities == True")
+            .drop(["filtered_cities"], axis=1)
         )
