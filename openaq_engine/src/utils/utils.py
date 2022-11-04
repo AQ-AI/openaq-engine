6eaa37c082968630333982c2225b14f709af08f2
ChristinaLast <christina.last@outlook.com>
2022-11-04 15:32:30 +0000

    adding all fixes to branch

--- openaq_engine/src/utils/utils.py
+++ openaq_engine/src/utils/utils.py
@@ -1,11 +1,13 @@
 import time
-import os
-
+import json
+from typing import Any, List
+from pydantic.json import pydantic_encoder
 import boto3
+import numpy as np
 from datetime import timedelta
-from typing import Any
 
 import pandas as pd
+from setup_environment import connect_to_db
 
 
 def date_range(start, end):
@@ -49,11 +51,6 @@ def write_csv(df: pd.DataFrame, path: str, **kwargs: Any) -> None:
 
 
 def query_results(params, query, wait=True):
-    s3 = boto3.resource(
-        "s3",
-        aws_access_key_id=os.getenv("ACCESS_ID"),
-        aws_secret_access_key=os.getenv("ACCESS_KEY"),
-    )
     session = boto3.Session()
 
     client = session.client("athena", params["region"])
@@ -62,7 +59,11 @@ def query_results(params, query, wait=True):
         QueryString=query,
         QueryExecutionContext={"Database": "default"},
         ResultConfiguration={
-            "OutputLocation": "s3://" + params["bucket"] + "/" + params["path"] + "/"
+            "OutputLocation": "s3://"
+            + params["bucket"]
+            + "/"
+            + params["path"]
+            + "/"
         },
     )
 
@@ -78,30 +79,114 @@ def query_results(params, query, wait=True):
         while iterations > 0:
             iterations = iterations - 1
             response_get_query_details = client.get_query_execution(
-                QueryExecutionId=response_query_execution_id["QueryExecutionId"]
+                QueryExecutionId=response_query_execution_id[
+                    "QueryExecutionId"
+                ]
             )
-            status = response_get_query_details["QueryExecution"]["Status"]["State"]
+            status = response_get_query_details["QueryExecution"]["Status"][
+                "State"
+            ]
 
             if (status == "FAILED") or (status == "CANCELLED"):
-                failure_reason = response_get_query_details["QueryExecution"]["Status"][
-                    "StateChangeReason"
-                ]
+                failure_reason = response_get_query_details["QueryExecution"][
+                    "Status"
+                ]["StateChangeReason"]
                 print(failure_reason)
                 return False, False
 
             elif status == "SUCCEEDED":
-                location = response_get_query_details["QueryExecution"][
-                    "ResultConfiguration"
-                ]["OutputLocation"]
-
-                ## Function to get output results
+                # Function to get output results
                 response_query_result = client.get_query_results(
-                    QueryExecutionId=response_query_execution_id["QueryExecutionId"]
+                    QueryExecutionId=response_query_execution_id[
+                        "QueryExecutionId"
+                    ]
                 )
-                # result_data = response_query_result["ResultSet"]
-                return response_query_result, location
+                return response_query_result
 
         else:
-            time.sleep(5)
+            time.sleep(0.001)
 
         return False
+
+
+def get_s3_file_path_list(resource, bucket, folder):
+    csv_filetype = ".csv"
+    my_bucket = resource.Bucket(bucket)
+    csv_list = []
+    for object_summary in my_bucket.objects.filter(Prefix=f"{folder}"):
+        if object_summary.key.endswith(csv_filetype):
+            csv_list.append(object_summary.key)
+
+    return csv_list
+
+
+def write_dataclass(dclass: object, path: str) -> None:
+    """
+    Write a dataclass to the provided path as a json
+
+    """
+    with open(path, "w+") as f:
+        f.write(
+            json.dumps(
+                dclass, indent=4, ensure_ascii=True, default=pydantic_encoder
+            )
+        )
+
+
+def get_categorical_feature_indices(df: pd.DataFrame) -> List[int]:
+    return list(np.where(df.dtypes == "category")[0])
+
+
+def json_provider(file_path, cmd_name):
+    with open(file_path) as config_data:
+        return json.load(config_data)
+
+
+def parametrized(dec):
+    def layer(*args, **kwargs):
+        def repl(f):
+            return dec(f, *args, **kwargs)
+
+        return repl
+
+    return layer
+
+
+def get_data(query):
+    """
+    Pulls data from the db based on the query
+    Input
+    -----
+    query: str
+       SQL query from the database
+    Output
+    ------
+    data: DataFrame
+       Dump of Query into a DataFrame
+    """
+
+    with connect_to_db() as conn:
+        df = pd.read_sql_query(query, conn)
+    return df
+
+
+def write_to_db(
+    df,
+    engine,
+    table_name,
+    schema_name,
+    table_behaviour,
+    index=False,
+    **kwargs,
+):
+    #     with engine.begin() as connection:
+    #         connection.execute(text("""SET ROLE "pakistan-ihhn-role" """))
+
+    df.to_sql(
+        name=table_name,
+        schema=schema_name,
+        con=engine,
+        if_exists=table_behaviour,
+        index=index,
+        **kwargs,
+    )
