83a2c1258629f4d7eea9d3dfe9259ea59b5b36ed
ChristinaLast <christina.last@outlook.com>
2022-11-18 09:16:23 -0500

    linting

--- openaq_engine/src/features/satellite/_ee_data.py
+++ openaq_engine/src/features/satellite/_ee_data.py
@@ -1,6 +1,5 @@
-import datetime
-import re
-from typing import List, Sequence, Tuple
+import logging
+from typing import List, Tuple
 
 import ee
 from ee.ee_exception import EEException
@@ -35,24 +34,11 @@ class EEData:
         )
 
     def execute(self, engine, save_images):
-        start_date_query = """SELECT {date_col} AS datetime
-        FROM {table} ORDER BY {date_col} ASC limit 1;""".format(
-            table=self.table_name,
-            date_col=self.date_col,
-        )
-        end_date_query = """SELECT {date_col} AS datetime
-        FROM {table} ORDER BY {date_col} DESC limit 1;""".format(
-            table=self.table_name,
-            date_col=self.date_col,
-        )
         ee.Authenticate()
-        end_date = get_data(end_date_query)
-        start_date = get_data(start_date_query)
-        s_datetime = self._generate_date(str(start_date["datetime"][0]))
-        e_datetime = self._generate_date(str(end_date["datetime"][0]))
+        end_date, start_date = self._generate_timerange()
         Parallel(n_jobs=-1, backend="multiprocessing", verbose=5)(
             delayed(self.execute_for_collection)(
-                collection, image_bands, s_datetime, e_datetime, save_images
+                collection, image_bands, start_date, end_date, save_images
             )
             for collection, image_bands in self.all_satellites
         )
@@ -81,12 +67,20 @@ class EEData:
             a boolean flag whether to write satellite data to google storage
         """
         ee.Initialize()
-        collection = (
-            ee.ImageCollection(collection)
-            .select(image_bands)
-            .filterDate(s_datetime, e_datetime)
-        )
-        print(collection)
+        try:
+            image_collection = (
+                ee.ImageCollection(collection)
+                .select(image_bands)
+                .filterDate(s_datetime, e_datetime)
+            )
+        except (EEException, HttpError):
+            logging.warning(
+                f"""Image collection {image_collection.getInfo()}
+                does not match any existing location."""
+            )
+            pass
+
+        logging.info(collection)
         down_args = {
             "image": collection,
             "bucket": self.bucket_name,
@@ -98,9 +92,17 @@ class EEData:
             task = ee.batch.Export.image.toCloudStorage(**down_args)
             task.start()
 
-    def _generate_date(self, date_str) -> datetime.date:
-        """Get datetime from string"""
-        date = datetime.datetime.strptime(
-            re.search("(?<=utc=)(.*)(?=, )", date_str).group(0), "%Y-%m-%dT%H:%M:%S.%fZ"
+    def _generate_timerange(self) -> Tuple[str]:
+        start_date_query = """SELECT {date_col} AS datetime
+        FROM {table} ORDER BY {date_col} ASC limit 1;""".format(
+            table=self.table_name,
+            date_col=self.date_col,
         )
-        return date
+        end_date_query = """SELECT {date_col} AS datetime
+        FROM {table} ORDER BY {date_col} DESC limit 1;""".format(
+            table=self.table_name,
+            date_col=self.date_col,
+        )
+        end_date = str(get_data(end_date_query)[0])
+        start_date = str(get_data(start_date_query)[0])
+        return end_date, start_date
