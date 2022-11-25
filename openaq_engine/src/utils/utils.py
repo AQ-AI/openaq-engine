a5cbec6b5f787ce154914b69c17b081af90cb786
ChristinaLast <christina.last@outlook.com>
2022-11-25 07:46:42 -0500

    adding ee_to_array

--- openaq_engine/src/utils/utils.py
+++ openaq_engine/src/utils/utils.py
@@ -53,7 +53,11 @@ def query_results(params, query, wait=True):
         QueryString=query,
         QueryExecutionContext={"Database": "default"},
         ResultConfiguration={
-            "OutputLocation": "s3://" + params["bucket"] + "/" + params["path"] + "/"
+            "OutputLocation": "s3://"
+            + params["bucket"]
+            + "/"
+            + params["path"]
+            + "/"
         },
     )
 
@@ -69,21 +73,27 @@ def query_results(params, query, wait=True):
         while iterations > 0:
             iterations = iterations - 1
             response_get_query_details = client.get_query_execution(
-                QueryExecutionId=response_query_execution_id["QueryExecutionId"]
+                QueryExecutionId=response_query_execution_id[
+                    "QueryExecutionId"
+                ]
             )
-            status = response_get_query_details["QueryExecution"]["Status"]["State"]
+            status = response_get_query_details["QueryExecution"]["Status"][
+                "State"
+            ]
 
             if (status == "FAILED") or (status == "CANCELLED"):
-                failure_reason = response_get_query_details["QueryExecution"]["Status"][
-                    "StateChangeReason"
-                ]
+                failure_reason = response_get_query_details["QueryExecution"][
+                    "Status"
+                ]["StateChangeReason"]
                 print(failure_reason)
                 return False, False
 
             elif status == "SUCCEEDED":
                 # Function to get output results
                 response_query_result = client.get_query_results(
-                    QueryExecutionId=response_query_execution_id["QueryExecutionId"]
+                    QueryExecutionId=response_query_execution_id[
+                        "QueryExecutionId"
+                    ]
                 )
                 return response_query_result
 
@@ -111,7 +121,9 @@ def write_dataclass(dclass: object, path: str) -> None:
     """
     with open(path, "w+") as f:
         f.write(
-            json.dumps(dclass, indent=4, ensure_ascii=True, default=pydantic_encoder)
+            json.dumps(
+                dclass, indent=4, ensure_ascii=True, default=pydantic_encoder
+            )
         )
 
 
@@ -172,3 +184,27 @@ def write_to_db(
         index=index,
         **kwargs,
     )
+
+
+def ee_array_to_df(arr, list_of_bands):
+    """Transforms client-side ee.Image.getRegion array to pandas.DataFrame."""
+    df = pd.DataFrame(arr)
+
+    # Rearrange the header.
+    headers = df.iloc[0]
+    df = pd.DataFrame(df.values[1:], columns=headers)
+
+    # Remove rows without data inside.
+    df = df[["longitude", "latitude", "time", *list_of_bands]].dropna()
+
+    # Convert the data to numeric values.
+    for band in list_of_bands:
+        df[band] = pd.to_numeric(df[band], errors="coerce")
+
+    # Convert the time field into a datetime.
+    df["datetime"] = pd.to_datetime(df["time"], unit="ms")
+
+    # Keep the columns of interest.
+    df = df[["longitude", "latitude", "time", "datetime", *list_of_bands]]
+
+    return df
