01f9a99233ee6ad5e02b78c45b109a0c38162376
ChristinaLast <christina.last@outlook.com>
2022-11-24 23:27:53 -0500

    adding build-features cli command

--- openaq_engine/main.py
+++ openaq_engine/main.py
@@ -1,9 +1,13 @@
 import click
 
-from config.model_settings import CohortBuilderConfig, EEConfig, TimeSplitterConfig
+from config.model_settings import (
+    CohortBuilderConfig,
+    BuildFeaturesConfig,
+    TimeSplitterConfig,
+)
 from setup_environment import get_dbengine
 from src.cohort_builder import CohortBuilder
-from src.features.satellite._ee_data import EEData
+from src.features.build_features import BuildFeaturesRandomForest
 from src.time_splitter import TimeSplitter
 
 
@@ -27,13 +31,13 @@ class CohortBuilderFlow:
         )
 
 
-class EEDataFlow:
+class BuildFeaturesFlow:
     def __init__(self):
-        self.config = EEConfig()
+        self.config = BuildFeaturesConfig()
 
     def execute(self):
         # Trigger the authentication flow.
-        return EEData.from_dataclass_config(self.config)
+        return BuildFeaturesRandomForest.from_dataclass_config(self.config)
 
 
 @click.command("time-splitter", help="Splits csvs for time splits")
@@ -55,10 +59,23 @@ def cohort_builder():
 
 @click.command("feature-builder", help="Generate features for cohorts")
 def feature_builder():
+    engine = get_dbengine()
+
+    build_features = BuildFeaturesFlow().execute()
+    build_features.execute(engine)
+
+
+@click.command("run-pipeline", help="Run all pipeline")
+def run_pipeline():
     # initialize engine
     engine = get_dbengine()
-    ee_data = EEDataFlow().execute()
-    ee_data.execute(engine, save_images=True)
+    time_splitter = TimeSplitterFlow().execute()
+    train_validation_dict = time_splitter.execute()
+
+    cohort_builder = CohortBuilderFlow().execute()
+    df = cohort_builder.execute(train_validation_dict, engine)
+    build_features = BuildFeaturesFlow().execute()
+    build_features.execute(df, save_images=False)
 
 
 @click.group("openaq-engine", help="Library to query openaq data")
