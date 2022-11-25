8c71d441ba823b40ce3a0450adc6a8f17e215c34
ChristinaLast <christina.last@outlook.com>
2022-11-25 07:45:53 -0500

    linting

--- openaq_engine/src/preprocessing/filter.py
+++ openaq_engine/src/preprocessing/filter.py
@@ -5,8 +5,9 @@ import pandas as pd
 
 class Filter:
     @staticmethod
-    def filter_pollutant(df: pd.DataFrame, pollutant_to_predict: str) -> pd.DataFrame:
-
+    def filter_pollutant(
+        df: pd.DataFrame, pollutant_to_predict: str
+    ) -> pd.DataFrame:
         """
         Filter for rows selected pollutant
 
@@ -20,7 +21,8 @@ class Filter:
             df.assign(
                 selected_pollutant=(
                     df.parameter.apply(
-                        lambda pollutant: pollutant_to_predict in str(pollutant)
+                        lambda pollutant: pollutant_to_predict
+                        in str(pollutant)
                     )
                 )
             )
@@ -30,7 +32,6 @@ class Filter:
 
     @staticmethod
     def filter_no_coordinates(df: pd.DataFrame) -> pd.DataFrame:
-
         """
         Filter for rows selected pollutant
 
@@ -41,14 +42,16 @@ class Filter:
         """
         return (
             df.assign(
-                no_coords=(df.coordinates.apply(lambda coords: str(coords) == "{}"))
+                no_coords=(
+                    df.coordinates.apply(lambda coords: str(coords) == "{}")
+                )
             )
             .query("no_coords == False")
             .drop(["no_coords"], axis=1)
         )
 
     @staticmethod
-    def filter_non_null_pm25_values(df: pd.DataFrame) -> pd.DataFrame:
+    def filter_non_null_values(df: pd.DataFrame) -> pd.DataFrame:
         """
         Filter out rows which are non null
 
@@ -69,7 +72,7 @@ class Filter:
         )
 
     @staticmethod
-    def filter_extreme_pollution_values(df: pd.DataFrame) -> pd.DataFrame:
+    def filter_extreme_values(df: pd.DataFrame) -> pd.DataFrame:
         """
         Filter out rows which contain extremely high pm25 values
 
@@ -90,7 +93,9 @@ class Filter:
         )
 
     @staticmethod
-    def filter_countries(df: pd.DataFrame, countries: List[str]) -> pd.DataFrame:
+    def filter_countries(
+        df: pd.DataFrame, countries: List[str]
+    ) -> pd.DataFrame:
         """
         Filter for countries
 
@@ -105,7 +110,8 @@ class Filter:
                 filtered_country=(
                     df.country.apply(
                         lambda country: any(
-                            str_ in country[1:-1].split(",") for str_ in countries
+                            str_ in country[1:-1].split(",")
+                            for str_ in countries
                         )
                     )
                 )
