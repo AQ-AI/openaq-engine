e04ffd3e5800a0eb32d7de8464cfdb029bcf62ce
ChristinaLast <christina.last@outlook.com>
2022-11-25 00:06:48 -0500

    adding satellite extraction

--- openaq_engine/src/features/satellite/_ee_data.py
+++ openaq_engine/src/features/satellite/_ee_data.py
@@ -4,25 +4,31 @@ from typing import List, Tuple
 import ee
 from ee.ee_exception import EEException
 from googleapiclient.errors import HttpError
+from geetools import batch
+import pandas as pd
 from joblib import Parallel, delayed
 
 from config.model_settings import EEConfig
-from src.utils.utils import get_data
+from src.utils.utils import get_data, ee_array_to_df
 
 
-class EEData:
+class EEVariableFeatures:
     def __init__(
         self,
         date_col: int,
         table_name: int,
         all_satellites: zip(List[str], List[str]),
         bucket_name: str,
+        path_to_private_key: str,
+        service_account: str,
     ):
 
         self.date_col = date_col
         self.table_name = table_name
         self.all_satellites = all_satellites
         self.bucket_name = bucket_name
+        self.path_to_private_key = path_to_private_key
+        self.service_account = service_account
 
     @classmethod
     def from_dataclass_config(cls, config: EEConfig) -> "EEData":
@@ -31,21 +37,78 @@ class EEData:
             table_name=config.TABLE_NAME,
             all_satellites=config.ALL_SATELITTES,
             bucket_name=config.BUCKET_NAME,
+            path_to_private_key=config.PATH_TO_PRIVATE_KEY,
+            service_account=config.SERVICE_ACCOUNT,
         )
 
-    def execute(self, engine, save_images):
+    def execute(self, df, save_images):
         ee.Authenticate()
-        end_date, start_date = self._generate_timerange()
-        Parallel(n_jobs=-1, backend="multiprocessing", verbose=5)(
-            delayed(self.execute_for_collection)(
-                collection, image_bands, start_date, end_date, save_images
-            )
-            for collection, image_bands in self.all_satellites
-        )
+        # end_date, start_date = self._generate_timerange()
+        satellite_df = pd.concat(
+            Parallel(n_jobs=-1, backend="multiprocessing", verbose=5)(
+                delayed(self.execute_for_location)(lon, lat, day, save_images)
+                for lon, lat, day in zip(df.x, df.y, df.day)
+            ),
+        ).reset_index(drop=True)
+        features_df = self.generate_features(satellite_df)
+
+        print(features_df)
 
     def execute_for_collection(
-        self, collection, image_bands, s_datetime, e_datetime, save_images
+        self,
+        collection,
+        image_bands,
+        save_images,
     ):
+        """
+        Input
+        ----
+        Takes the name of a satellite image collection and the bands
+        (fields) to extract data for, and for a given date range.
+
+        Arguments:
+        ----
+        collection:
+            A str of satellite to query
+        image_bands:
+            A list of bands captured for each satellite
+        save_images:
+            a boolean flag whether to write satellite data to google storage
+        """
+        ee.Initialize()
+
+        # logging.info(
+        #     "please sigup to Google Earth Engine here:"
+        #     " https://signup.earthengine.google.com/"
+        # )
+        # if bucket.blob(f"{collection}_{s_datetime}_{e_datetime}"):
+        try:
+            logging.info(f"Downloading: {collection}")
+
+            image_collection = ee.ImageCollection(collection).select(
+                image_bands
+            )
+
+            if save_images is True:
+                down_args = {
+                    "image": image_collection,
+                    "bucket": self.bucket_name,
+                    "description": f"{collection}",
+                    "scale": 30,
+                }
+
+                task = batch.Export.image.toCloudStorage(**down_args)
+                task.start()
+            else:
+                return image_collection
+        except (EEException, HttpError):
+            logging.warning(
+                f"""Image collection {image_collection.getInfo()}
+                does not match any existing location."""
+            )
+            pass
+
+    def execute_for_location(self, lon, lat, day, save_images):
         """
         Input
         ----
@@ -57,40 +120,49 @@ class EEData:
         ----
         collection:
             A str of satellite to query
-        image_bands:
-            A list of bands captured for each satellite
-        s_datetime:
-            the start date to extract satellite images for
-        e_datetime:
-            the end date to extract satellite images for
-        save_images:
-            a boolean flag whether to write satellite data to google storage
+        lon:
+            the longitude of a  sensor location
+        lat:
+            the latitude of a sensor location
+        datetime:
+            the date the sensor reading was taken
         """
-        ee.Initialize()
-        try:
-            image_collection = (
-                ee.ImageCollection(collection)
-                .select(image_bands)
-                .filterDate(s_datetime, e_datetime)
+        df_list = []
+        for collection, image_bands, period, resolution in self.all_satellites:
+            image_collection = self.execute_for_collection(
+                collection,
+                image_bands,
+                save_images,
             )
-        except (EEException, HttpError):
-            logging.warning(
-                f"""Image collection {image_collection.getInfo()}
-                does not match any existing location."""
+            day_of_interest = ee.Date(day)
+            centroid_point = ee.Geometry.Point(lon, lat)
+            satellite_value = self._get_value_from_collection(
+                image_collection,
+                day_of_interest,
+                centroid_point,
+                period,
+                resolution,
             )
-            pass
+            try:
+                ee_df = ee_array_to_df(satellite_value, image_bands)
+                ee_df["x"] = lon
+                ee_df["y"] = lat
+                ee_df["date"] = day
+                df_list.append(ee_df)
+            except IndexError:
+                pass
+        return pd.concat(df_list)
 
-        logging.info(collection)
-        down_args = {
-            "image": collection,
-            "bucket": self.bucket_name,
-            "description": f"{collection}_{s_datetime}_{e_datetime}",
-            "scale": 30,
-        }
+    def generate_features(self, satellite_df):
+        features_df = (
+            satellite_df.drop(
+                labels=["time", "datetime", "longitude", "latitude"], axis=1
+            )
+            .groupby(["x", "y", "date"], as_index=False)
+            .agg(["mean"])
+        )
 
-        if save_images is True:
-            task = ee.batch.Export.image.toCloudStorage(**down_args)
-            task.start()
+        print(features_df)
 
     def _generate_timerange(self) -> Tuple[str]:
         start_date_query = """SELECT {date_col} AS datetime
@@ -103,6 +175,29 @@ class EEData:
             table=self.table_name,
             date_col=self.date_col,
         )
-        end_date = str(get_data(end_date_query)[0])
-        start_date = str(get_data(start_date_query)[0])
+        end_date = str(get_data(end_date_query)["datetime"][0])
+        start_date = str(get_data(start_date_query)["datetime"][0])
         return end_date, start_date
+
+    def _get_value_from_collection(
+        self,
+        image_collection,
+        day_of_interest,
+        centroid_point,
+        period,
+        resolution,
+    ):
+        try:
+            filtered_image_collection = image_collection.filterDate(
+                day_of_interest, day_of_interest.advance(period, "day")
+            )
+            info = filtered_image_collection.getRegion(
+                centroid_point, resolution
+            ).getInfo()
+            return info
+        except (EEException, HttpError):
+            # logging.warning(
+            #     f"""Centroid location and date does not
+            #     match any existing ee.Image."""
+            # )
+            pass
