bba13f62fc1c43a4a9834839903576323839b283
lastc <lastc@mit.edu>
2024-05-21 17:40:53 +0100

    fix: merge conflicts

--- openaq_engine/src/features/satellite/_ee_data.py
+++ openaq_engine/src/features/satellite/_ee_data.py
@@ -44,6 +44,7 @@ class EEFeatures:
             service_account=config.SERVICE_ACCOUNT,
         )
 
+
     def execute(self, df, save_images):
         ee.Authenticate()
         # end_date, start_date = self._generate_timerange()
@@ -57,7 +58,10 @@ class EEFeatures:
 
         print(features_df)
 
-    def execute_for_location(self, lon, lat, day, save_images):
+
+    def execute_for_location(
+        self, location_id, lon, lat, date_utc, cohort, save_images
+    ):
         """
         Input
         ----
@@ -98,39 +102,13 @@ class EEFeatures:
                 period,
                 resolution,
             )
-            try:
-                ee_df = ee_array_to_df(satellite_value, image_bands)
-                ee_df["x"] = lon
-                ee_df["y"] = lat
-                ee_df["date"] = day
+            if not ee_df.empty:
                 df_list.append(ee_df)
-            except IndexError:
-                pass
-        for (
-            collection,
-            image_bands,
-            resolution,
-        ) in self.static_satellites:
-            image_collection = self.execute_for_collection(
-                collection,
-                image_bands,
-                save_images,
-            )
-            satellite_value = self._get_value_from_static_collection(
-                image_collection,
-                day_of_interest,
-                centroid_point,
-                resolution,
-            )
-            try:
-                ee_df = ee_array_to_df(satellite_value, image_bands)
-                ee_df["x"] = lon
-                ee_df["y"] = lat
-                ee_df["date"] = day
-                df_list.append(ee_df)
-            except IndexError:
-                pass
-        return pd.concat(df_list)
+        try:
+            return pd.concat(df_list).reset_index(drop=True)
+        except ValueError:
+            return pd.DataFrame()
+
 
     def execute_for_collection(
         self,
@@ -220,22 +198,332 @@ class EEFeatures:
     def _get_value_from_variable_collection(
         self,
         image_collection,
-        day_of_interest,
-        centroid_point,
+        image_bands,
+        location_id,
+        date_utc,
+        lon,
+        lat,
+        cohort,
         period,
         resolution,
     ):
+        """
+        This function builds an algorithm to compute
+        the representative satellite value for a sensor location,
+        considering only the exact location and time of flyover (within the hour).
+        """
         try:
-            filtered_image_collection = image_collection.filterDate(
-                day_of_interest, day_of_interest.advance(period, "day")
+            ee_df = self.get_satellite_data_within_hour(
+                image_collection,
+                image_bands,
+                location_id,
+                lon,
+                lat,
+                cohort,
+                resolution,
+                date_utc,
             )
-            info = filtered_image_collection.getRegion(
-                centroid_point, resolution
-            ).getInfo()
-            return info
-        except (EEException, HttpError):
-            # logging.warning(
-            #     f"""Centroid location and date does not
-            #     match any existing ee.Image."""
-            # )
-            pass
+            if not ee_df.empty:
+                logging.info("Getting satellite data within the hour of interest")
+                print("Hourly image: ", ee_df)
+                return ee_df
+            else:
+                logging.warn(f"No matching satellite data within the hour for {lon}, {lat} at {date_utc}")
+                return pd.DataFrame()
+        except (EEException, HttpError) as e:
+            logging.error(f"Error retrieving satellite data: {e}")
+            return pd.DataFrame()
+
+    def get_satellite_data_within_hour(
+        self,
+        image_collection,
+        image_bands,
+        location_id,
+        lon,
+        lat,
+        cohort,
+        resolution,
+        date_utc,
+    ):
+        """
+        This function takes in an image collection and a set of spatial and temporal parameters
+        to calculate the satellite value for a sensor location within the hour of the sensor reading.
+        """
+        centroid_point = ee.Geometry.Point(lon, lat)
+        sensor_datetime = datetime.datetime.strptime(date_utc, "%Y-%m-%dT%H:%M:%S.%fZ")
+        start_of_hour = sensor_datetime.replace(minute=0, second=0, microsecond=0)
+        end_of_hour = start_of_hour + datetime.timedelta(hours=1)
+
+        filtered_image_collection = image_collection.filterDate(
+            ee.Date(start_of_hour.isoformat()), ee.Date(end_of_hour.isoformat())
+        )
+
+        info = filtered_image_collection.getRegion(
+            centroid_point, resolution
+        ).getInfo()
+
+        return self._create_satellite_dataframe(
+            info, image_bands, location_id, date_utc, lon, lat, cohort
+        )
+
+
+    def get_satellite_data_within_hour(
+        self,
+        image_collection,
+        image_bands,
+        location_id,
+        lon,
+        lat,
+        cohort,
+        resolution,
+        date_utc,
+    ):
+        """
+        This function takes in an image collection and a set of spatial and temporal parameters
+        to calculate the satellite value for a sensor location within the hour of the sensor reading.
+        """
+        centroid_point = ee.Geometry.Point(lon, lat)
+        sensor_datetime = datetime.datetime.strptime(date_utc, "%Y-%m-%dT%H:%M:%S.%fZ")
+        start_of_hour = sensor_datetime.replace(minute=0, second=0, microsecond=0)
+        end_of_hour = start_of_hour + datetime.timedelta(hours=1)
+
+        filtered_image_collection = image_collection.filterDate(
+            ee.Date(start_of_hour.isoformat()), ee.Date(end_of_hour.isoformat())
+        )
+
+        info = filtered_image_collection.getRegion(
+            centroid_point, resolution
+        ).getInfo()
+
+        return self._create_satellite_dataframe(
+            info, image_bands, location_id, date_utc, lon, lat, cohort
+        )
+
+
+    def get_most_recent_satellite_data(
+        self,
+        image_collection,
+        image_bands,
+        location_id,
+        lon,
+        lat,
+        cohort,
+        resolution,
+        date_utc,
+        period,
+    ):
+        """
+        This function takes in an image collection
+        and a set of spatial and temporal parameters
+        to calculate the weighted temporal average
+        value for each satellite query given a time period.
+
+        Arguments
+        -------
+        image_collection: str
+            the string of an image collection
+        image_bands: List[str]
+            the list of image bands used (satellite model features)
+        location_id: str
+            the location_id of the sensor
+        lon: float
+            Longitude of sensor
+        lat: float
+            Latitude of sensor
+        resolution: float
+            resolution of image and used as satellite search radius
+        date_utc: datetime
+            datetime in utc of sensor reading
+        period: int
+            the number of days between satellite passovers
+        """
+        centroid_point = ee.Geometry.Point(lon, lat)
+        day_of_interest = ee.Date(date_utc)
+
+        filtered_image_collection = image_collection.filterDate(
+            day_of_interest.advance(-period, "days"), day_of_interest
+        )
+
+        info = filtered_image_collection.getRegion(
+            centroid_point, resolution
+        ).getInfo()
+
+        return self._create_satellite_dataframe(
+            info,
+            image_bands,
+            location_id,
+            date_utc,
+            lon,
+            lat,
+            cohort,
+        )
+
+    def get_satellite_data_within_lookback(
+        self,
+        image_collection,
+        image_bands,
+        location_id,
+        lon,
+        lat,
+        cohort,
+        resolution,
+        date_utc,
+        period,
+    ):
+        """
+        This function takes in an image collection
+        and a set of spatial and temporal parameters
+        to calculate the weighted temporal average
+        value for each satellite within a lookback.
+        """
+
+        centroid_point = ee.Geometry.Point(lon, lat)
+        day_of_interest = ee.Date(date_utc)
+
+        filtered_image_collection = image_collection.filterDate(
+            day_of_interest.advance(-(self.lookback_n * period), "days"),
+            day_of_interest,
+        )
+        info = filtered_image_collection.getRegion(
+            centroid_point, resolution
+        ).getInfo()
+        return self._create_satellite_dataframe(
+            info, image_bands, location_id, date_utc, lon, lat, cohort
+        )
+
+    def get_any_recent_satellite_data(
+        self,
+        image_collection,
+        image_bands,
+        location_id,
+        lon,
+        lat,
+        cohort,
+        resolution,
+        date_utc,
+    ):
+        """This function collects all satellite imagery from
+        between the specified date and the first date for a specific
+        geolocation with no time windor specified"""
+        centroid_point = ee.Geometry.Point(lon, lat)
+        day_of_interest = ee.Date(date_utc)
+        start_date = ee.Date(
+            "2015-01-01",
+        )
+        filtered_image_collection = image_collection.filterDate(
+            start_date,
+            day_of_interest,
+        )
+        filtered_image_collection = image_collection.limit(10)
+        info = filtered_image_collection.getRegion(
+            centroid_point, resolution
+        ).getInfo()
+        return self._create_satellite_dataframe(
+            info, image_bands, location_id, date_utc, lon, lat, cohort
+        )
+
+    def _create_satellite_dataframe(
+        self,
+        info,
+        image_bands,
+        location_id,
+        date_utc,
+        lon,
+        lat,
+        cohort,
+    ):
+        """Creates a dataframe from returned satellite information and
+        builds required fields for weighted average calculation"""
+        ee_df = ee_array_to_df(info, image_bands)
+        ee_df = self._calculate_temporal_weighted_average(date_utc, ee_df)
+        ee_df = self._calculate_spatial_weighted_average(
+            ee_df, lon, lat, location_id
+        )
+        ee_df["cohort"] = cohort
+        ee_df["timestamp_utc"] = date_utc
+        return ee_df
+
+    def _calculate_temporal_weighted_average(self, date_utc, ee_df):
+        """Calculate the difference between the sensor timestamp and the
+        satellite timestamp for all returned values"""
+        ee_df["sensor_datetime"] = datetime.datetime.strptime(
+            date_utc, "%Y-%m-%dT%H:%M:%S.%fZ"
+        )
+        ee_df["sensor_timestamp"] = ee_df.sensor_datetime.astype("int64")
+        ee_df["satellite_timestamp"] = ee_df.datetime.astype("int64")
+
+        ee_df["timestamp_diff"] = (
+            ee_df["sensor_timestamp"] - ee_df["satellite_timestamp"]
+        )
+        return ee_df
+
+    def _calculate_spatial_weighted_average(
+        self, ee_df, lon, lat, location_id
+    ):
+        """Calculate the spatially-weighted distance"""
+        ee_df["sensor_longitude"] = lon
+        ee_df["sensor_latitude"] = lat
+        ee_df["location_id"] = location_id
+        ee_df["distance"] = ee_df.apply(self.calculate_distance, axis=1)
+        return ee_df
+
+    def calculate_distance(self, row):
+        try:
+            return haversine(
+                (row["sensor_latitude"], row["sensor_longitude"]),
+                (row["latitude"], row["longitude"]),
+                unit="m",
+            )
+        except Exception as e:
+            print(f"Error processing row {row}: {e}")
+            return None  # or appropriate error value
+
+    def _weighted_mean_by_lambda(
+        self, df, avg_cols, weight_cols, groupby_cols
+    ):
+        def _scale_weight_cols(df, weight_cols):
+            """This takes in a DataFrame and columns used to construct
+            a weight column using the MinMaxScalar() function"""
+            scaler = MinMaxScaler()
+            df[weight_cols] = scaler.fit_transform(df[weight_cols])
+            df["weight"] = df.loc[:, weight_cols].prod(axis=1)
+            return df
+
+        def _weighted_means_by_column_ignoring_NaNs(x, cols, w="weights"):
+            """This takes a DataFrame and averages each data column (cols),
+            weighting observations by column w, but ignoring individual NaN
+            observations within each column.
+            """
+            try:
+                return pd.Series(
+                    [
+                        np.nan
+                        if x.dropna(subset=[c]).empty
+                        else np.average(
+                            x.dropna(subset=[c])[c],
+                            weights=x.dropna(subset=[c])[w],
+                        )
+                        for c in cols
+                    ],
+                    cols,
+                )
+            except ZeroDivisionError:
+                pd.Series(
+                    [
+                        np.nan
+                        if x.dropna(subset=[c]).empty
+                        else np.average(
+                            x.dropna(subset=[c])[c],
+                        )
+                        for c in cols
+                    ],
+                    cols,
+                )
+
+        df = _scale_weight_cols(df, weight_cols)
+
+        return (
+            df.groupby(groupby_cols)
+            .apply(_weighted_means_by_column_ignoring_NaNs, avg_cols, "weight")
+            .reset_index()
+        )
--- openaq_engine/src/preprocess.py
+++ openaq_engine/src/preprocess.py
@@ -120,17 +120,28 @@ class Preprocess:
         """
         Extract timezone into "utc" and "local" timezone columns.
         """
+        utc_time_str = re.search(r"(?<=utc=)(.*?)(?=,)", row["date"]).group(0)
+        local_time_str = re.search(r"(?<=local=)(.*?)(?=})", row["date"]).group(0)
+
+        utc_time = datetime.fromisoformat(utc_time_str).replace(tzinfo=timezone.utc)
+        local_time = datetime.fromisoformat(local_time_str)
+
+        row["timestamp_utc"] = utc_time.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+        row["timestamp_local"] = local_time.strftime("%Y-%m-%dT%H:%M:%S%z")
+        return row
+
+    def _extract_timestamp_from_api(self, row: pd.Series) -> pd.Series:
+        """
+        Extract timezone into "utc" and "local" timezone columns from dict.
+        """
+        date_info = row["date"]
+
+        # If date_info is a string, convert it to a dictionary
+        if isinstance(date_info, str):
+            date_info = json.loads(date_info)
+
         row["timestamp_utc"] = (
-            datetime.fromisoformat(
-                re.search("(?<=utc=)(.*)(?=,)", row["date"]).group(0)[:-1]
-            )
-            .astimezone(timezone.utc)
-            .strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-        )
-        row["timestamp_local"] = (
-            datetime.fromisoformat(
-                re.search("(?<=local=)(.*)(?=})", row["date"]).group(0),
-            )
+            datetime.fromisoformat(date_info["utc"].replace("Z", "+00:00"))
             .astimezone(timezone.utc)
             .strftime("%Y-%m-%dT%H:%M:%S%z")
         )
