87b5fa903754adb2baf2ac7354183d99024f708b
ChristinaLast <christina.last@outlook.com>
2024-05-22 21:18:16 +0000

    style: linting

--- openaq_engine/mlflows/cli/features/build_features.py
+++ openaq_engine/mlflows/cli/features/build_features.py
@@ -6,53 +6,21 @@ from config.model_settings import BuildFeaturesConfig
 
 
 @parametrized
-def feature_builder_options(fn, countries_option: bool = True):
+def feature_builder_options(fn):
     """
-    countries_option: bool = True
-        Whether to provide the option to specify countries or not
+    Modify the CLI options to include cohort-table.
     """
     cohort_builder_config = OptionGroup(
         "Options for defining the cohort",
-        help=(
-            "Allows definition of custom cohorts for provided "
-            "countries and from the date provided"
-        ),
+        help="Allows definition of custom cohorts using the provided cohort table",
     )
-    country_ = cohort_builder_config.option(
-        "-c",
-        "--country",
-        default=BuildFeaturesConfig.COUNTRY,
+
+    cohort_table = cohort_builder_config.option(
+        "-t",
+        "--cohort-table",
+        default=BuildFeaturesConfig.TABLE_NAME,
         type=click.STRING,
-        help=(
-            "Load timesplits from specific countries in the 'Country Code'"
-            " format e.g. 'IN' (India)"
-        ),
+        help="Name of the table containing cohort data",
     )
-    pollutant = cohort_builder_config.option(
-        "-p",
-        "--pollutant",
-        default=BuildFeaturesConfig.TARGET_VARIABLE,
-        type=click.Choice(
-            [
-                "co",
-                "no2",
-                "o3",
-                "pm1",
-                "pm10",
-                "pm25",
-                "so2",
-            ]
-        ),
-        help="Load timesplits from data for the pollutant requested",
-    )
-    latest_date = cohort_builder_config.option(
-        "-d",
-        "--latest-date",
-        hidden=True,
-        type=click.STRING,
-        help="Date to load data until in format YYYY-MM-DD",
-    )
-    wrapped_func = pollutant(latest_date(fn))
-    if countries_option:
-        wrapped_func = country_(wrapped_func)
-    return wrapped_func
+
+    return cohort_table(fn)
