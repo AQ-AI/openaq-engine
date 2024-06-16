f62d6b6d0850c1a15c821f7c582c1fd7a54fc586
ChristinaLast <christina.last@outlook.com>
2024-06-16 18:05:15 +0000

    fix: linting

--- tests/create_test_database.py
+++ tests/create_test_database.py
@@ -11,7 +11,11 @@ def setup_test_database():
     pg_host = os.getenv("PGHOST", "localhost")
     pg_port = os.getenv("PGPORT", "5432")
 
-    # Connect to the default postgres database to create the test_db
+    test_db_user = "test_user"
+    test_db_password = "test_password"
+    test_db_name = "test_db"
+
+    # Connect to the default postgres database to create the test_db and user
     try:
         con = psycopg2.connect(
             dbname="postgres",
@@ -22,7 +26,19 @@ def setup_test_database():
         )
         con.autocommit = True
         cur = con.cursor()
-        cur.execute("CREATE DATABASE test_db")
+
+        # Create test database
+        cur.execute(f"CREATE DATABASE {test_db_name}")
+        print("Database created successfully.")
+
+        # Create test user
+        cur.execute(
+            f"DO $$ BEGIN IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '{test_db_user}') THEN CREATE ROLE {test_db_user} WITH LOGIN PASSWORD '{test_db_password}'; END IF; END $$;"
+        )
+        print("User created successfully.")
+
+        cur.execute(f"ALTER ROLE {test_db_user} WITH SUPERUSER;")
+
         cur.close()
         con.close()
     except psycopg2.errors.DuplicateDatabase:
@@ -31,7 +47,7 @@ def setup_test_database():
         )
 
     # Connect to the test_db as superuser
-    test_db_url = f"postgresql://{superuser}:{superuser_password}@{pg_host}:{pg_port}/test_db"
+    test_db_url = f"postgresql://{superuser}:{superuser_password}@{pg_host}:{pg_port}/{test_db_name}"
     test_engine = create_engine(test_db_url, isolation_level="AUTOCOMMIT")
 
     with test_engine.connect() as connection:
@@ -120,14 +136,16 @@ def setup_test_database():
 
         # Grant all privileges on the test_db to test_user
         connection.execute(
-            text("GRANT ALL PRIVILEGES ON DATABASE test_db TO test_user")
+            text(
+                f"GRANT ALL PRIVILEGES ON DATABASE {test_db_name} TO {test_db_user}"
+            )
         )
         print("Granted all privileges on test_db to test_user.")
 
         # Grant all privileges on all tables in test_db to test_user
         connection.execute(
             text(
-                "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO test_user"
+                f"GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO {test_db_user}"
             )
         )
         print("Granted all privileges on all tables in test_db to test_user.")
