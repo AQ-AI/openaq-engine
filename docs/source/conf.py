5aecb79b69056916ad79e45294e3c60f7b994396
ChristinaLast <christina.last@outlook.com>
2024-08-01 04:44:38 +0000

    style: removing unused import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,6 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
-import sys
 
 print("We are here!!!!!!!!!!: ")
 os.chdir(os.path.abspath('../../openaq_engine'))
