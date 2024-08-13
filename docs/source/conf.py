5c49adeb3728e2824cc9081d84d9b651c9940950
ChristinaLast <christina.last@outlook.com>
2024-08-13 11:30:36 +0000

    feat: updating conf.py for sphinx documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,78 +1,35 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 import os
-
-print("We are here!!!!!!!!!!: ")
-os.chdir(os.path.abspath('../../openaq_engine'))
+import sys
 
 
-#sys.path.append('/home/workspace/myproj/myproj')
-#older version
-#sys.path.insert(0, os.path.abspath('../../openaq_engine'))
-print("WE are hereeee "+ os.getcwd())
+# Add the openaq_engine directory to the Python path
+# Adjust the sys.path to point to the correct directory
+sys.path.insert(0, os.path.abspath('../../openaq_engine'))
 
+# # Add the config and mlflows directories to the Python path if needed
+# sys.path.insert(0, os.path.abspath('../openaq_engine/config'))
+# sys.path.insert(0, os.path.abspath('../openaq_engine/mlflows'))
+
+print("Current working directory: " + os.getcwd())
+print("Python path includes: " + str(sys.path))
 
 # -- Project information -----------------------------------------------------
-
 project = "openaq-engine"
 copyright = "2024, Christina Last, Prithviraj Pramanik"
 author = "Christina Last, Prithviraj Pramanik"
-
-# The full version, including alpha/beta/rc tags
 release = "0.1.0"
 
-
 # -- General configuration ---------------------------------------------------
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+]
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = ['sphinx.ext.autodoc',
-     'sphinx.ext.autosummary',
-      'sphinx.ext.napoleon',
-        'sphinx.ext.viewcode',
-    ]
 autosummary_generate = True
-
-
-
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-
 # -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = "alabaster"
-# html_theme = 'sphinx_rtd_theme'
-
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ["_static"]
-
-# extensions = [
-#     'sphinx.ext.autodoc',
-#     'sphinx.ext.viewcode',  # Optional: If you want to include links to source code
-#     'sphinx.ext.napoleon',  # Optional: If you use Google or NumPy style docstrings
-#     # Add other extensions here
-# ]
-
